f7b9f362261215d753e780188594badfa522bb45
Ashwin19v <ashwinash190105@gmail.com>
2024-10-02 09:05:22 +0530

    d

---
+++ lib/pages/todo_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoDetails extends StatelessWidget {
+  const TodoDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
